b9d7937256f6b65a4ee59ffb319220f142ea5bf2
SayantaniDutta-sys <sayantanidutta06@gmail.com>
2025-03-11 17:32:35 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 11 16:53:07 2025
+
+@author: 91912
+"""
+
+n = int(input("Enter a number: "))
+
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
